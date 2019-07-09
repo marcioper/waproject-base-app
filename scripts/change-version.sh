b31e1d3c87bff8dc3d1dfdc86c62ee3260eaf991
danieloprado <danielprado.ad@gmail.com>
2019-07-09 11:25:17 -0300

    fix fastlane
    Signed-off-by: danieloprado <danielprado.ad@gmail.com>

--- scripts/change-version.sh
+++ scripts/change-version.sh
@@ -3,4 +3,6 @@ set -e
 BASEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 (cd $BASEDIR/../ && yarn)
 
-node $BASEDIR/change-version.js -f $1
+GIT_COMMIT="$(git rev-list --all --count)"
+
+node $BASEDIR/change-version.js -f $GIT_COMMIT 
